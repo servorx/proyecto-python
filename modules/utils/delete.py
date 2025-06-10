51f1e9f559e63d9b700128ae9bbb8fda4c64ea6a
servorx <191423123+servorx@users.noreply.github.com.>
2025-06-10 17:27:07 -0500

    feat: :poop: temp

--- modules/utils/delete.py
+++ modules/utils/delete.py
@@ -20,7 +20,53 @@ def validate_value():
 
 # Función para manejar el proceso de eliminar según la sección (title o id)
 def delete_input(section):
-  pass
+  delete_item = input(f"Enter the {section} of the item you want to delete\n-> ")
+  # validacion en caso de que sea id, tiene que ser un valor entero
+  if section == "id":
+    try:
+      delete_item = int(delete_item)
+    except ValueError:
+      print("Invalid ID. It must be a number.")
+      pause_screen()
+      return delete()
+  
+  # data es como tal todo el recorrido de todo del archivo principal
+  data = read_json(DB_FILE)
+  found = None  
+  data_found = None  
+
+  for section_name in data:
+    for index, element in enumerate(data[section_name]):
+      if element.get(section) == delete_item:
+        found = (section_name, index)
+        data_found = element
+        break
+    if found:
+      break
+
+  if found:
+    try:
+      # todo: hacer que esto imprima con tabulate
+      confirm = input(f"\nThis is the data you wrote: {data_found}\nDo you want to delete that? (y/n): ").lower()
+      if confirm == "y":
+        success = delete_json(DB_FILE, [found[0], found[1]])
+        if success:
+          print("Data deleted successfully!")
+        else:
+          print("Something went wrong while trying to delete.")
+      elif confirm == "n":
+        print("Changes discarded.")
+      else:
+        print("This is not a valid value, please enter again.")
+    except Exception as e:
+      print(f"Error: {e}. Make sure you typed the data correctly.")
+    finally:
+      pause_screen()
+      return delete()
+  else:
+    print("You wrote a non-existent item.")
+    pause_screen()
+    return delete_input(section)
 
 # Función principal de eliminar el dato de acuerdo a la entrada del usuario
 def delete():
@@ -30,11 +76,9 @@ def delete():
     return delete()
   match value:
     case 1:
-      section = "title"
-      delete_input(section)
+      delete_input("title")
     case 2:
-      section = "id"
-      delete_input(section)
+      delete_input("id")
     case 3:
       return
     case _:
